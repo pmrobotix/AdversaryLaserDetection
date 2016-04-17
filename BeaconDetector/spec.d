72e78ffdfd1873c685e72fb9f441a0f193ce9540
steph-m <steph-m@users.noreply.github.com>
2016-04-17 16:19:40 +0200

    Add eclispe metadata

---
+++ BeaconDetector/spec.d
@@ -0,0 +1,2 @@
+spec.o: \
+ D:/PM-ROBOTIX/eclipseArduino/workspace/.metadata/.plugins/org.eclipse.cdt.managedbuilder.core/spec.C
--- BeaconDetectorLedPanel/.ino.cpp
+++ BeaconDetectorLedPanel/.ino.cpp
@@ -1,7 +1,7 @@
 //This is a automatic generated file
 //Please do not modify this file
 //If you touch this file your change will be overwritten during the next build
-//This file has been generated on 2016-04-17 15:03:17
+//This file has been generated on 2016-04-17 16:15:24
 
 #include "Arduino.h"
 #include <Rainbowduino.h>
